4c029a6d223de57d243cb7278d5ef1103ac18d38
Robert Fuchs <robertfuchsyoshi@gmail.com>
2022-11-17 23:54:42 -0600

    Add sides count control to WebUI

--- web.rb
+++ web.rb
@@ -4,13 +4,13 @@ require 'model'
 require 'parser'
 require 'texture'
 
-def generate_zip(file, name, size, color)
-  size = size.to_f
+def generate_zip(file, name, radius, color, sides)
+  radius = radius.to_f
 
   verts = Parser.parse_log(file)
   vtf, vmt = Texture.generate_unlit_color(color, "bp-gen/botpath")
   vtf64 = Base64.encode64(vtf)
-  smd, qc = Model.generate_model(verts, name, size / 2, "botpath-#{color}", "bp-gen", 6)
+  smd, qc = Model.generate_model(verts, name, radius, "botpath-#{color}", "bp-gen", sides)
 
   return [smd, qc, vtf64, vmt]
 end
